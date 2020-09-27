589a5c26cbd64e553c09c6dbb5f4cd984671a362
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2020-09-27 13:15:11 +0300

    Other cameras fixed and exit function added.

--- Assets/Game/Scripts/WinScene.cs
+++ Assets/Game/Scripts/WinScene.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinScene : MonoBehaviour
 {
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Application.Quit();
+            SceneManager.LoadScene("GameplayScene");
         }
     }
 }
