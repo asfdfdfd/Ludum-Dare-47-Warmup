f6f301f16d2b3ec61ec92953d870e1329b09f775
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2020-09-27 13:03:45 +0300

    Gameplay camera fixed.

---
+++ Assets/Game/Scripts/CameraFollowsBubble.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowsBubble : MonoBehaviour
+{
+    public GameObject gameObjectBubble;
+
+    private float deltaY;
+
+    private void Start()
+    {
+        deltaY = gameObject.transform.position.y - gameObjectBubble.transform.position.y;
+    }
+
+    private void FixedUpdate()
+    {
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObjectBubble.transform.position.y + deltaY, -10);
+    }
+}
