0187bc3559412b384d679403b30b278dcfaa611b
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2020-10-02 22:34:11 +0300

    Voice acting added.

---
+++ Assets/Game/Scripts/StartupSound.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class StartupSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] sounds;
+
+    private AudioSource _audioSource;
+    
+    private Random random = new Random();
+    
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        _audioSource.clip = sounds[random.Next(0, sounds.Length)];
+        _audioSource.Play();
+    }
+}
